e8d30c16043ff18c95b69624a616beb39fdc2751
Matheus-H-Mx <74945355+Matheus-H-Mx@users.noreply.github.com>
2021-06-25 02:57:56 -0300

    add Pie and Cardify

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -1,7 +1,6 @@
 //
 //  EmojiMemoryGameView.swift
 //  Memorize
-//
 //  Created by Matheus Henrique on 12/05/21.
 //
 
@@ -23,39 +22,47 @@ struct EmojiMemoryGameView: View {
  }
 
 struct Cardview: View {
-    var card:MemoryGame<String>.Card
+    var card: MemoryGame<String>.Card
 
     var body: some View {
         GeometryReader { geometry in
             self.body(for: geometry.size)
         }
     }
-      
+      @ViewBuilder
        private func body(for size: CGSize) -> some View {
-            ZStack {
-                if card.isFaceUp {
-                RoundedRectangle(cornerRadius:cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius:cornerRadius).stroke(lineWidth: edgeLineWidth)
-                    Text(card.content)
-                } else {
-                    if !card.isMatched {
-                    RoundedRectangle(cornerRadius:cornerRadius).fill()
-                    }
-                }
+        if card.isFaceUp || !card.isMatched {
+        ZStack{
+            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                .padding().opacity(0.3)
+            Text(card.content)
+                .font(Font.system(size: fontSize(for: size)))
             }
-            .font(Font.system(size: fontSize(for: size)))
+            .cardify(isFaceUp: card.isFaceUp)
         }
-    
-    
+    }
+            
     //MARK: - Drawing Constants
     
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth:CGFloat = 3
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.75
+        
     }
 }
-    
+
+
+
+
+
+
+struct EmojiMemoryGameView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[4])
+        return EmojiMemoryGameView(viewModel: game)
+    }
+}
     
   
     
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -1,30 +1,32 @@
 //
 //  MemoryGame.swift
 //  Memorize
-//
 //  Created by Matheus Henrique on 13/05/21.
 //
 
 import Foundation
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
-    private (set) var cards: Array<Card>
+    var cards: (Array<Card>)
+    var numMatches: Int
     
     var indexOfTheOneAndOnlyFaceUpCard: Int? {
-        get {cards.indices.filter { cards[$0].isFaceUp }.only }
+        get {cards.indices.filter { cards[$0].isFaceUp }.only}
         set {
             for index in cards.indices {
-                    cards[index].isFaceUp = index == newValue
+                cards[index].isFaceUp = index == newValue
             }
         }
     }
     
     mutating func choose(card: Card) {
-        if let chosenIndex = cards.firstindex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
+        print("card chosen: \(card)")
+        if let chosenIndex: Int = cards.firstIndex(matching:card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched {
             if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                     cards[chosenIndex].isMatched = true
                     cards[potentialMatchIndex].isMatched = true
+                    numMatches += 1
                 }
                 self.cards[chosenIndex].isFaceUp = true
             } else {
@@ -35,12 +37,14 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = Array<Card>()
+        numMatches = 0
+        
         for pairIndex in 0..<numberOfPairsOfCards {
-        let content = cardContentFactory(pairIndex)
-        cards.append(Card(content: content, id: pairIndex*2))
-        cards.append(Card(content: content, id: pairIndex*2+1))
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
     }
-}
     
     struct Card: Identifiable {
         var isFaceUp: Bool = false
@@ -49,4 +53,3 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         var id: Int
     }
 }
-
